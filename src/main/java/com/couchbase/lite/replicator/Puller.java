5ad8a0cfce098e8eaf3806d386f9821fa4074bb0
Alex Trotsenko <olexii.trotsenko@gmail.com>
2014-08-07 16:49:48 +0200

    avoid stucking continuous replication on re-start: fully remove continuous replication from db om stop.

--- src/main/java/com/couchbase/lite/replicator/Puller.java
+++ src/main/java/com/couchbase/lite/replicator/Puller.java
@@ -107,10 +107,11 @@ public final class Puller extends Replication implements ChangeTrackerClient {
             changeTracker.setClient(null);  // stop it from calling my changeTrackerStopped()
             changeTracker.stop();
             changeTracker = null;
-            if (!continuous) {
-                Log.v(Log.TAG_SYNC, "%s | %s : puller.stop() calling asyncTaskFinished()", this, Thread.currentThread());
-                asyncTaskFinished(1);  // balances asyncTaskStarted() in beginReplicating()
-            }
+            Log.v(Log.TAG_SYNC, "%s | %s : puller.stop() calling asyncTaskFinished()", this, Thread.currentThread());
+	        /* Required to stop continuous replication, that synced nothing from remote db yet.
+	         * For continuous replication, that already synced something from remote db, this lead to "asyncTaskCount = -1",
+	         * but it's "ok" since we stopping replication and it will not be used anymore.*/
+	        asyncTaskFinished(1);  // balances asyncTaskStarted() in beginReplicating()
         }
 
         synchronized (this) {
